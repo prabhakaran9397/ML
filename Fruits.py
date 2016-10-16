437844caa367c23cb7e224842d07a05a31aa4a42
prabhakaran9397 <prabhakaran9397@gmail.com>
2016-10-17 05:12:09 +0530

    bug fixed

--- Fruits.py
+++ Fruits.py
@@ -1,10 +1,9 @@
-#!/usr/bin/python
-
 #BEGIN
 from sklearn import tree
 import csv
 import re
 Dataset     = csv.reader(open("Data",  "rb"))
+Int         = "^[0-9]+$"
 Features    = []
 Labels      = []
 Index       = 0
@@ -12,10 +11,13 @@ NumberRepo  = {}
 for row in Dataset:
     Arr  = []
     for i in range(len(row)-1):
-        NumberRepo.setdefault(row[i], Index)
-        Arr.append(NumberRepo[row[i]])
-        if NumberRepo[row[i]] == Index:
-            Index += 1
+        if re.search(Int, row[i]):
+            Arr.append(int(row[i]))
+        else:
+            NumberRepo.setdefault(row[i], Index)
+            Arr.append(NumberRepo[row[i]])
+            if NumberRepo[row[i]] == Index:
+                Index += 1
     Features.append(Arr) 
     NumberRepo.setdefault(row[len(row)-1], Index)
     Labels.append(NumberRepo[row[len(row)-1]])
@@ -26,6 +28,6 @@ classifier  = tree.DecisionTreeClassifier().fit(Features, Labels)
 
 #DRIVER
 weight      = input("Whats the weight? ")
-texture     = input("smooth(0) or bumpy(1)? ")
+texture     = NumberRepo.get(raw_input("smooth or bumpy? "), -1)
 predicted   =  classifier.predict([[weight, texture]])
 print NumberRepo.keys()[NumberRepo.values().index(predicted[0])]
